44f2e0758cccacd98725965f62286976a961340d
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-03-23 15:00:10 +0900

    feat: splittingをtrueにした

--- workspaces/client/tsup.config.ts
+++ workspaces/client/tsup.config.ts
@@ -59,7 +59,7 @@ export default defineConfig(async (): Promise<Options[]> => {
       platform: 'browser',
       shims: true,
       sourcemap: 'inline',
-      splitting: false,
+      splitting: true,
       target: ['es2022'],
       treeshake: false,
     },
