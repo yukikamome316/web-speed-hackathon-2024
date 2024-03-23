0fc0214c0f202fdfa84a87cbfb93b2fe303d3efa
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-03-23 14:52:58 +0900

    fix: treeshakeをfalseに戻した

--- workspaces/client/tsup.config.ts
+++ workspaces/client/tsup.config.ts
@@ -61,7 +61,7 @@ export default defineConfig(async (): Promise<Options[]> => {
       sourcemap: 'inline',
       splitting: false,
       target: ['es2022'],
-      treeshake: true,
+      treeshake: false,
     },
   ];
 });
