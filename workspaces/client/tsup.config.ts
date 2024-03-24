1e628fe47792e5193bbe8081a4d44b4f6fd1b7c8
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-03-24 16:38:26 +0900

    feat: sourcemapを消した

--- workspaces/client/tsup.config.ts
+++ workspaces/client/tsup.config.ts
@@ -58,7 +58,7 @@ export default defineConfig(async (): Promise<Options[]> => {
       outDir: OUTPUT_DIR,
       platform: 'browser',
       shims: true,
-      sourcemap: 'inline',
+      // sourcemap: 'inline',
       splitting: true,
       target: ['esnext'],
       // treeshake: false,
